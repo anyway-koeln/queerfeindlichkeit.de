da204601db6d45928f2d66e633fcddf6e29ed223
thomasrosen <thomas.rosen@me.com>
2021-04-04 21:06:39 +0200

    added react-helmet-async to set head stuff from react

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { Helmet } from 'react-helmet-async'
 import Header from './components/Header.js'
 import Rainbow from './components/Rainbow.js'
 
 function App() {
   return (
     <>
+      <Helmet>
+        <title>QueerFeindlichkeit</title>
+        <link rel="canonical" href="https://queerfeindlichkeit.de/" />
+        <meta name="description" content="Hier entsteht ein Antidiskriminierungsprojekt des anyway Köln zum Thema Queerfeindlichkeit." />
+      </Helmet>
+
       <Header />
 
       <p style={{textAlign: 'center', height: '50vh', margin: '64px 0'}}>

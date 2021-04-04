e002bf26b1bc7e92faeb6a81cd24c502ff170c8b
thomasrosen <thomas.rosen@me.com>
2021-04-04 20:40:07 +0200

    added option for light and dark rainbow variants

--- src/components/Rainbow.js
+++ src/components/Rainbow.js
@@ -1,13 +1,24 @@
 import classes from './Rainbow.module.css'
 
-function Rainbow({ children, ...props }) {
+function Rainbow({ children, variant, ...props }) {
+
+  variant = (
+    variant === 'light'
+    ? '-light'
+    : (
+        variant === 'dark'
+        ? '-dark'
+        : '' // normal = nothing
+    )
+  )
+
   return <div className={classes.rainbow} {...props}>
-    <div className={classes.stripe} style={{ backgroundColor: 'var(--rainbow-red)' }}></div>
-    <div className={classes.stripe} style={{ backgroundColor: 'var(--rainbow-orange)' }}></div>
-    <div className={classes.stripe} style={{ backgroundColor: 'var(--rainbow-yellow)' }}></div>
-    <div className={classes.stripe} style={{ backgroundColor: 'var(--rainbow-green)' }}></div>
-    <div className={classes.stripe} style={{ backgroundColor: 'var(--rainbow-blue)' }}></div>
-    <div className={classes.stripe} style={{ backgroundColor: 'var(--rainbow-purple)' }}></div>
+    <div className={classes.stripe} style={{ backgroundColor: `var(--rainbow${variant}-red)` }}></div>
+    <div className={classes.stripe} style={{ backgroundColor: `var(--rainbow${variant}-orange)` }}></div>
+    <div className={classes.stripe} style={{ backgroundColor: `var(--rainbow${variant}-yellow)` }}></div>
+    <div className={classes.stripe} style={{ backgroundColor: `var(--rainbow${variant}-green)` }}></div>
+    <div className={classes.stripe} style={{ backgroundColor: `var(--rainbow${variant}-blue)` }}></div>
+    <div className={classes.stripe} style={{ backgroundColor: `var(--rainbow${variant}-purple)` }}></div>
   </div>
 }
 

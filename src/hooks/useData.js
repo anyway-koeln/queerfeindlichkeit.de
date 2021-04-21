7b36b4b19a5e6359fe1cd0c970218dee96b39d6b
thomasrosen <thomas.rosen@me.com>
2021-04-21 14:47:26 +0200

    added text property to the sample data

--- src/hooks/useData.js
+++ src/hooks/useData.js
@@ -10,20 +10,20 @@ export default function useData(region) {
       setData({
         region,
         articles: [
-          { id: Math.random(), title: 'Artikel 1', date_published: now },
-          { id: Math.random(), title: 'Artikel 2', date_published: now },
-          { id: Math.random(), title: 'Artikel 3', date_published: now },
-          { id: Math.random(), title: 'Artikel 4', date_published: now },
-          { id: Math.random(), title: 'Artikel 5', date_published: now },
-          { id: Math.random(), title: 'Artikel 6', date_published: now },
+          { id: Math.random(), title: 'Artikel 1', date_published: now, text: 'Lorem ipsum…' },
+          { id: Math.random(), title: 'Artikel 2', date_published: now, text: 'Lorem ipsum…' },
+          { id: Math.random(), title: 'Artikel 3', date_published: now, text: 'Lorem ipsum…' },
+          { id: Math.random(), title: 'Artikel 4', date_published: now, text: 'Lorem ipsum…' },
+          { id: Math.random(), title: 'Artikel 5', date_published: now, text: 'Lorem ipsum…' },
+          { id: Math.random(), title: 'Artikel 6', date_published: now, text: 'Lorem ipsum…' },
         ],
         stories: [
-          { id: Math.random(), title: 'Story 1', date_published: now },
-          { id: Math.random(), title: 'Story 2', date_published: now },
-          { id: Math.random(), title: 'Story 3', date_published: now },
-          { id: Math.random(), title: 'Story 4', date_published: now },
-          { id: Math.random(), title: 'Story 5', date_published: now },
-          { id: Math.random(), title: 'Story 6', date_published: now },
+          { id: Math.random(), title: 'Story 1', date_published: now, text: 'Lorem ipsum…' },
+          { id: Math.random(), title: 'Story 2', date_published: now, text: 'Lorem ipsum…' },
+          { id: Math.random(), title: 'Story 3', date_published: now, text: 'Lorem ipsum…' },
+          { id: Math.random(), title: 'Story 4', date_published: now, text: 'Lorem ipsum…' },
+          { id: Math.random(), title: 'Story 5', date_published: now, text: 'Lorem ipsum…' },
+          { id: Math.random(), title: 'Story 6', date_published: now, text: 'Lorem ipsum…' },
         ],
       })
     }, 100)

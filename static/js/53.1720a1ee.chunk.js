93e0111212ea8de13adade28e7563bcf125087b6
thomasrosen <thomas.rosen@me.com>
2021-04-22 11:37:05 +0000

    thomasrosen published a site update

---
+++ static/js/53.1720a1ee.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpapp=this.webpackJsonpapp||[]).push([[53],{162:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return c}));var i=e(15),o=e(13),c=function(){var n=window;n.addEventListener("statusTap",(function(){Object(i.h)((function(){var t=n.innerWidth,e=n.innerHeight,c=document.elementFromPoint(t/2,e/2);if(c){var r=c.closest("ion-content");r&&new Promise((function(n){return Object(o.c)(r,n)})).then((function(){Object(i.f)((function(){return r.scrollToTop(300)}))}))}}))}))}}}]);
+//# sourceMappingURL=53.1720a1ee.chunk.js.map

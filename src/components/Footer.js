a1e0bbc1576fa11a6fc7f12038a6c854f2356f53
thomasrosen <thomas.rosen@me.com>
2021-04-23 16:26:43 +0200

    BUGFIX correct GitHub link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
     <footer className="body1">
       <a href="https://www.instagram.com/queerfeindlichkeit/">Instagram: @queerfeindlichkeit</a><br />
       <a href="https://twitter.com/anyway_koeln">Twitter: @anyway_koeln</a><br />
-      <a href="https://github.com/anyway_koeln/">Source Code on GitHub</a><br />
+      <a href="https://github.com/anyway-koeln/">Source Code on GitHub</a><br />
       <br />
       <a href="https://www.anyway-koeln.de/">Dies ist ein Projekt des anyway Köln</a><br />
       <a href="https://www.anyway-koeln.de/kontakt/">Kontakt</a><br />

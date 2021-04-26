d2e03a1cb86ce6433fda291e91317e5ebfeb680c
thomasrosen <thomas.rosen@me.com>
2021-04-26 16:06:50 +0200

    open links to external websites in a new tab

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,9 @@ import './Footer.module.css'
 function Footer() {
   return (
     <footer className="body1">
-      <a href="https://www.instagram.com/queerfeindlichkeit/">Instagram: @queerfeindlichkeit</a><br />
-      <a href="https://twitter.com/anyway_koeln">Twitter: @anyway_koeln</a><br />
-      <a href="https://github.com/anyway-koeln/">Source Code on GitHub</a><br />
+      <a href="https://www.instagram.com/queerfeindlichkeit/" target="_blank">Instagram: @queerfeindlichkeit</a><br />
+      <a href="https://twitter.com/anyway_koeln" target="_blank">Twitter: @anyway_koeln</a><br />
+      <a href="https://github.com/anyway-koeln/" target="_blank">Source Code on GitHub</a><br />
       <br />
       <a href="https://www.anyway-koeln.de/">Dies ist ein Projekt des anyway Köln</a><br />
       <a href="https://www.anyway-koeln.de/kontakt/">Kontakt</a><br />

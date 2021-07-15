7c2b90b5803b0bc67a42a33d1d03da47d4f134f6
thomasrosen <thomas.rosen@me.com>
2021-07-15 15:48:42 +0200

    display stats

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,14 @@
 import AktivWerden from './HomeSections/AktivWerden.js'
 import ArticleRow from './HomeSections/ArticleRow.js'
 import Forderungen from './HomeSections/Forderungen.js'
-// import Statistics from './HomeSections/Statistics.js'
+import Statistics from './HomeSections/Statistics.js'
 
 function Home() {
   return <>
     <AktivWerden />
     <ArticleRow />
     <Forderungen />
-    {/* <Statistics /> */}
+    <Statistics />
   </>
 }
 
